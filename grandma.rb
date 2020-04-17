c90500736278ddd9e200232fed855b67db799ae6
Teinelle Charles <teinelledcharles@gmail.com>
2020-04-17 07:33:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+end
+
+def speak_to_grandma
  elsif phrase !== phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
 else phrase == "NO, NOT SINCE 1938!"
